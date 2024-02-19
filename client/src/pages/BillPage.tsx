c6767a46b33ca911ba1d3116f9a7883da4f2680f
AmritHimanshu <amrit.ewp@gmail.com>
2024-02-19 16:29:30 +0530

    added save the bill

--- client/src/pages/BillPage.tsx
+++ client/src/pages/BillPage.tsx
@@ -1,13 +1,203 @@
 import React from 'react';
 import Sidebar from './Sidebar';
-import ViewBillSection from './ViewBillSection';
 
 function BillPage() {
     return (
         <div className="home-container">
             <Sidebar />
             <div className="home-AddNew">
-                <ViewBillSection />
+                <div className="viewBillSection-container">
+                    <div className="viewBillSection-header">
+                        <div>
+                            <div className="title">
+                                Darjeeling
+                            </div>
+                            <div className="date">
+                                25/02/2024
+                            </div>
+                        </div>
+
+                        <div className="add-spends">
+                            Add Spends
+                        </div>
+                    </div>
+
+                    <div className="viewBillSection-card-container">
+                        <div className="viewBillSection-card">
+                            <div className="card-title">
+                                Ramesh
+                            </div>
+                            <div className="total-spent">
+                                <div>Total spent</div>
+                                <div>$25</div>
+                            </div>
+                            <hr />
+                            <div className="due">Due</div>
+
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                        </div>
+
+                        <div className="viewBillSection-card">
+                            <div className="card-title">
+                                Ramesh
+                            </div>
+                            <div className="total-spent">
+                                <div>Total spent</div>
+                                <div>$25</div>
+                            </div>
+                            <hr />
+                            <div className="due">Due</div>
+
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                        </div>
+
+                        <div className="viewBillSection-card">
+                            <div className="card-title">
+                                Ramesh
+                            </div>
+                            <div className="total-spent">
+                                <div>Total spent</div>
+                                <div>$25</div>
+                            </div>
+                            <hr />
+                            <div className="due">Due</div>
+
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                        </div>
+
+                        <div className="viewBillSection-card">
+                            <div className="card-title">
+                                Ramesh
+                            </div>
+                            <div className="total-spent">
+                                <div>Total spent</div>
+                                <div>$25</div>
+                            </div>
+                            <hr />
+                            <div className="due">Due</div>
+
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                        </div>
+
+                        <div className="viewBillSection-card">
+                            <div className="card-title">
+                                Ramesh
+                            </div>
+                            <div className="total-spent">
+                                <div>Total spent</div>
+                                <div>$25</div>
+                            </div>
+                            <hr />
+                            <div className="due">Due</div>
+
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                        </div>
+
+                        <div className="viewBillSection-card">
+                            <div className="card-title">
+                                Ramesh
+                            </div>
+                            <div className="total-spent">
+                                <div>Total spent</div>
+                                <div>$25</div>
+                            </div>
+                            <hr />
+                            <div className="due">Due</div>
+
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                            <div className="due-track">
+                                <div>Vikash</div>
+                                <div>$5</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- client/src/pages/NewHome.tsx
+++ client/src/pages/NewHome.tsx
@@ -1,13 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Sidebar from './Sidebar';
-import AddNew from './AddNew';
 
 function NewHome() {
+
+    const navigate = useNavigate();
+
+    const [title, setTitle] = useState("");
+    const [noOfInputs, setNoOfInputs] = useState([{ memberName: "" }, { memberName: "" }]);
+
+    const handleDeleteInput = (index: number) => {
+        const newArray = [...noOfInputs];
+        newArray.splice(index, 1);
+        setNoOfInputs(newArray);
+    }
+
+    const handleAddInput = () => {
+        setNoOfInputs([...noOfInputs, { memberName: "" }]);
+    }
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+        const { value } = e.target;
+
+        let onChangedValue = [...noOfInputs];
+        onChangedValue[index].memberName = value;
+
+        setNoOfInputs(onChangedValue);
+    }
+
+    const handleOnCreate = async (e: any) => {
+        e.preventDefault();
+        if (title === "") return window.alert("Enter the Title");
+        noOfInputs.map((item, index) => {
+            if (item.memberName === "") return window.alert(`Member ${index + 1} is empty`);
+        });
+
+        const memberNames = noOfInputs.map(input => input.memberName);
+        // console.log(memberNames);
+
+        try {
+            const res = await fetch('/createBill', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    title, memberNames
+                })
+            });
+
+            const data = await res.json();
+            if (res.status !== 200 || !data) {
+                return window.alert(`${data.error}`);
+            }
+            else {
+                console.log(data);
+                navigate('/view-bill');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
         <div className="home-container">
             <Sidebar />
             <div className="home-AddNew">
-                <AddNew />
+                <div className="addNew-container">
+                    <div className="addNew-container-title">
+                        Add New
+                    </div>
+                    <form>
+                        <div className="addNew-form-title">
+                            <label htmlFor="title">Title</label>
+                            <input id="title" name="title" type="text" placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        </div>
+
+                        {noOfInputs.map((item, index) => (
+                            <div key={index}>
+                                <div className="addNew-form-member">
+                                    <label htmlFor={`member${index + 1}`}>Member {index + 1}:</label>
+                                    <div className="addNew-form-member-input">
+                                        <input id={`member${index + 1}`} name={`member${index + 1}`} value={item.memberName} type="text" placeholder="Enter name" onChange={(e) => handleOnChange(e, index)} />
+
+                                        {index >= 2 && <DeleteIcon style={{ cursor: "pointer" }} onClick={() => handleDeleteInput(index)} />}
+                                    </div>
+                                </div>
+
+                                {index === noOfInputs.length - 1 &&
+                                    <div className="add-members-button" onClick={handleAddInput}>
+                                        {/* + Add Members */}
+                                        <AddCircleOutlineIcon style={{ marginRight: '5px' }} /> Add Members
+                                    </div>
+                                }
+                            </div>
+                        ))}
+
+                        <button className="addNew-create-button" onClick={handleOnCreate}>Create</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
