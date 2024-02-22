dc09b078c416c1040569412ead9949ee8d2c8619
AmritHimanshu <amrit.ewp@gmail.com>
2024-02-22 12:25:52 +0530

    solving cors error

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,13 +12,19 @@ const PORT = process.env.PORT || 5000;
 require('./db/conn');
 
 app.use(cors({
-    // origin: true,
-    // origin: "http://localhost:3000",
+    // origin:true,
     origin: "https://share-bill-lemon.vercel.app",
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    credentials: true,
+    credentials: true,  // Enable credentials (cookies)
 }));
+
+// app.use(cors({
+//     // origin: true,
+//     // origin: "http://localhost:3000",
+//     origin: "https://share-bill-lemon.vercel.app",
+//     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+//     credentials: true,
+// }));
 
 app.use(express.json());
 app.use(require("./router/auth"));
