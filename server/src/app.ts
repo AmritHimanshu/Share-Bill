06c4528e387d63090fb1cba87cc219c5c53cf4d9
AmritHimanshu <amrit.ewp@gmail.com>
2024-02-22 12:51:54 +0530

    solving cors error

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,6 +4,12 @@ import * as dotenv from 'dotenv';
 // import cors from 'cors';
 const cors = require('cors');
 
+app.use(cors({
+    // origin:true,
+    origin: "*",
+    // origin: "https://share-bill-lemon.vercel.app",
+    credentials: true,  // Enable credentials (cookies)
+}));
 
 dotenv.config({ path: __dirname + '../.env' });
 
@@ -11,12 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 require('./db/conn');
 
-app.use(cors({
-    // origin:true,
-    origin: "*",
-    // origin: "https://share-bill-lemon.vercel.app",
-    credentials: true,  // Enable credentials (cookies)
-}));
+
 
 // app.use(cors({
 //     // origin: true,
